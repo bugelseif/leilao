422da6a8e29a8b6466209411e0975148ac56564a
Bruna <bferreira.slv@gmail.com>
2022-09-29 19:58:36 -0300

    join grupo multicast

--- receiver1.py
+++ receiver1.py
@@ -1,17 +1,39 @@
 import socket
 import struct
+import sys
+import threading
+
+IP = '127.0.0.1'
+PORT = 50000
 
 MCAST_GRP = '224.1.2.3'
 MCAST_PORT = 5004
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def main():
+    print('Iniciando cliente...')
+    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    entra = input("Gostaria de entrar no grupo?")
+    if entra != 'sim':
+        sys.exit()
+    else:
+        clientSock.sendto(bytes('join', 'utf-8'), (IP, PORT))
+        pedido_join = clientSock.recv(1024)
+        print(f'Recebido: {pedido_join}', end='\n\n\n')
+        threading.Thread(target=grupo_receiver).start()
+    
 
-sock.bind(('', MCAST_PORT))
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+def grupo_receiver():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    sock.bind(('', MCAST_PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
-while True:
-    print('Aguardando mensagem')
-    print(sock.recv(10240))
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        print('Aguardando mensagem')
+        print(sock.recv(10240))
+
+if __name__ == '__main__':
+    main()
--- sender.py
+++ sender.py
@@ -1,15 +1,38 @@
 import socket
+import threading
+from time import sleep
 
-group = '224.1.2.3'
-port = 5004
+IP = '0.0.0.0'
+PORT = 50000
 
-ttl = 2
+MCAST_GRP = '224.1.2.3'
+MCAST_PORT = 5004
 
-sock = socket.socket(socket.AF_INET,
-                     socket.SOCK_DGRAM,
-                     socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP,
-                socket.IP_MULTICAST_TTL,
-                ttl)
-for i in range(3):
-    sock.sendto(b'ola!', (group, port))
+def main():
+    print('Iniciando servidor...', end='\n\n\n')
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSock.bind((IP, PORT))
+    while True:
+        pedido_join, client = serverSock.recvfrom(1024)
+        pedido_join = pedido_join.decode()
+        print(pedido_join)
+        serverSock.sendto(bytes('224.1.2.3', 'utf-8'), client)
+
+
+def grupo_sender():
+    ttl = 2
+    sock = socket.socket(socket.AF_INET,
+                        socket.SOCK_DGRAM,
+                        socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP,
+                    socket.IP_MULTICAST_TTL,
+                    ttl)
+    while True:
+        sock.sendto(b'Entrou no grupo', (MCAST_GRP, MCAST_PORT))
+        sleep(10)
+
+threading.Thread(target=grupo_sender).start()
+
+
+if __name__ == '__main__':
+    main()
